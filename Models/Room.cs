cd29e3e3a8df1d44758318f8879fe89391a2b9c3
Alex Semynin <alex.semynin1997@outlook.com>
2021-09-25 22:17:15 +0300

    Добавить модели

---
+++ Models/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Room
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public User Admin { get; set; }
+
+
+
+        public List<Message> Messages { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
