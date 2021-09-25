a20b09952a190904ffa6defed63768fcc13e455f
Alex Semynin <alex.semynin1997@outlook.com>
2021-09-25 22:20:45 +0300

    добавить контекст для работы с даными

---
+++ DataLayer/RepositoryChatContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataLayer
+{
+    public class RepositoryChatContext : DbContext
+    {
+        public RepositoryChatContext(DbContextOptions<RepositoryChatContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
