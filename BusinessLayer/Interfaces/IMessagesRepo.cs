13cffc784b1d2302a18e8d5eec082e44abd16858
Alex Semynin <alex.semynin1997@outlook.com>
2021-09-25 22:21:21 +0300

    Добавить репозитории для работы с данными

---
+++ BusinessLayer/Interfaces/IMessagesRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IMessagesRepo
+    {
+        Message CreateMessage(string content, string authorId);
+        Message GetMessage(string id);
+        List<Message> GetMessages(string[] ids);
+        List<Message> GetRoomMessages(string roomId);
+
+        Message UpdateMessage(string id, string content);
+        Message DeleteMessage(string id);
+    }
+}
---
+++ BusinessLayer/Interfaces/IUsersRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IUsersRepo
+    {
+        Task<User> CreateUser(string email, string password, DateTime birthDate, UserRoles role, string name);
+        User GetUser(string userId);
+
+        User UpdateUser(string userId, string email, string password, UserRoles role, string name);
+        User DeleteUser(string userId);
+
+    }
+}
