b887335521c98a381c625eb8153711479e362328
Alex Semynin <alex.semynin1997@outlook.com>
2021-09-25 22:22:58 +0300

    добавить контекст в виде сервиса (механизм внедрения зависимостей)

--- ChatWeb/Startup.cs
+++ ChatWeb/Startup.cs
@@ -3,19 +3,43 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using BusinessLayer;
+using DataLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BusinessLayer.Interfaces;
+using BusinessLayer.Implements;
 
 namespace ChatWeb
 {
     public class Startup
     {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<RepositoryChatContext>(options =>
+                options.UseSqlServer(connection)
+            );
+
+            services.AddTransient<IUsersRepo, EFUserRepository>();
+            services.AddTransient<IMessagesRepo, EFMessagesRepository>();
+            services.AddTransient<EFRoomsRepository, EFRoomsRepository>();
+
+            services.AddScoped<DataManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
