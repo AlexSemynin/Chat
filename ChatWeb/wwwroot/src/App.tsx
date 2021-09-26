a1f5f2f6ef0ee68fa0be9dcd3192af0d4f6bf6d7
Alex Semynin <alex.semynin1997@outlook.com>
2021-09-26 19:39:10 +0300

    добавить devextreme & devextreme-react

--- ChatWeb/wwwroot/src/App.tsx
+++ ChatWeb/wwwroot/src/App.tsx
@@ -1,7 +1,12 @@
 ﻿import React, { ReactNode } from "react";
+import 'devextreme/dist/css/dx.light.css';
+import Button from "devextreme-react/button";
 
 export const App = () => {
     return (
-        <h1> Hello! </h1>
+        <Button
+            type="success"
+            text="Click"
+        />
     )
 }
